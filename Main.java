a36916a5e05f39c1f7c906c7a607238b5dbf8c89
AndyRP1 <0960247@my.scccd.edu>
2024-10-01 12:08:40 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,50 +1,61 @@
-//ar 9/24/24
-//ArrayListPractice
+// ar 10/1/24
+// TodaysDate.ava
 
 
-import java.util.ArrayList;
+import java.util.Date;
+import java.text.SimpleDateFormat;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-
-        // Create an arraylist of strings
-        ArrayList<String> myListOfStrings = new ArrayList<>();
 
 
-        //Add a few elements to my ArrayList.
-        myListOfStrings.add("first");
-        myListOfStrings.add("Second");
-        myListOfStrings.add("third");
 
-        //Output our ArrayList
-        System.out.println("\n myListOfStrings is"+ myListOfStrings);
+        System.out.printf("\n Welcome to dates and animal class");
 
-        //Remove an element.
-        myListOfStrings.remove(0);
+        //Create a Date object to represent the current date
+        Date today =new Date ();
 
-        //Remove an element
-        myListOfStrings.remove("third");
+        //Define the desired date
+        SimpleDateFormat formatter = new SimpleDateFormat("yy-MM-dd")
 
-        //Output our ArrayList
-        System.out.println("\n After adding a bunch of stuff...");
-        System.out.println("\n myListOfStrings is: "+ myListOfStrings);
+        //Format the date and store it in a string
+        String strTodaysDate= formatter.format(today);
 
-        //Use a for loop to output each element of our arrayList
-        for (int i=0; i<myListOfStrings.size();i++){
-            System.out.println("An element in my list is:"+ myListOfStrings.get(i));
+        System.out.println(today's date in the format yyyy-MM-dd"+strTodaysDate);
+
+
+
+        //Write java code to calculate birthdates for the aimals and then create a method that returns
+        //fpr the animals birthdays
+
+
+        //input: "4 year old female hyena, born in spring"
+        //Processing : find the age and brith season
+        // 		and hen use decision control structures to
+        //		calculate the date.
+
+        //parse the string
+        String strStarting = "4 year old female hyena, born in spring, tan color, 70 pounds, from friguia";
+        //split the string on ","
+        String[]arrayOfStrPartsOncomma=strStarting.split(regex:,"")
+        //output the array elements
+        //this is a for :each loop- very handy when examining array elements.
+        for (String thePart: arrayofStrPartsOnComma){
+            Systen.out.Println("element of the String is :"+ thePart);
         }
+        String[ arrayofStrPartsOnSpace = arrayOfstrPartsOnComma[0].split(regex");
+        for (String thePart : arrayOfStrPartsOnSpace){
+            System.outPrintln("element of the string is "+ the part);
 
-        // use a for loop to add some things to our list
-        String toAdd = "";
-        for(int i=0;i<10;i++){
-            toAdd= toAdd + "adding...."+ Integer.toString(i);
-            myListOfStrings.add(toAdd);
+            String ageInYears = arrayOfStrPartsOnSpace[0]
+            String animalBirthSeason = arrayOfStrPartsOnSpace02[2];
+            System.out.println("the age in yeas of the animal is ;" +ageInYears);
+            System.out.println ("the seasons of birth of the animal is : animals birth season);
+
+
+                    String animalBirthdate=","
 
 
 
-
-
-        }
-    }
-}
+            if(animalBirthSeason.contains("spring")){
+                animalsBirthdate="2020-03-21"
