b0f3041f3ce5c18510a386267b05745ba842e7c1
AndyRP1 <0960247@my.scccd.edu>
2024-10-15 11:33:23 -0700

    Add files via upload

---
+++ Hyena.java
@@ -0,0 +1,10 @@
+public class Hyena extends andy.zoo.com.Animal {
+    // Create a static int that keep track of the number of hyenas created.
+    static int numOfHyenas = 0;
+
+    // Create a constructor.
+    public Hyena(){
+        super();
+        numOfHyenas++;
+    }
+}
